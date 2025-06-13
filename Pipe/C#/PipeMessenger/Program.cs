1add349a23ef5f4197418b8e96d36c0e3b4996f1
Cezary Danilowski <danilowski.cezary@gmail.com>
2025-06-13 07:03:31 +0200

    PipeMessenger update

--- Pipe/C#/PipeMessenger/Program.cs
+++ Pipe/C#/PipeMessenger/Program.cs
@@ -6,32 +6,39 @@ class Program
 {
     public static void Main()
     {
-        var pipeName = "test_pipe";
+        string pipeWriteName = @"\\.\pipe\write"; // client → server
+        string pipeReadName = @"\\.\pipe\read";   // server → client
 
-        using (var pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut))
+        using (var writeStream = new NamedPipeClientStream(".", "write", PipeDirection.Out))
+        using (var readStream = new NamedPipeClientStream(".", "read", PipeDirection.In))
         {
-            Console.WriteLine("Łączenie z serwerem pipe...");
-            pipeClient.Connect();
-            Console.WriteLine("Połączono!");
+            Console.WriteLine("Connecting to pipes...");
 
-            using (var sr = new StreamReader(pipeClient))
-            using (var sw = new StreamWriter(pipeClient) { AutoFlush = true })
+            writeStream.Connect();
+            Console.WriteLine("Connected to write pipe.");
+
+            readStream.Connect();
+            Console.WriteLine("Connected to read pipe.");
+
+            using (var sw = new StreamWriter(writeStream) { AutoFlush = true })
+            using (var sr = new StreamReader(readStream))
             {
-                string message = "";
-                string response = "";
+                string message;
+                string response;
 
                 do
                 {
+                    Console.Write("> ");
                     message = Console.ReadLine();
                     sw.WriteLine(message);
-                    Console.WriteLine("Wysłano: " + message);
+                    Console.WriteLine("[Sent] " + message);
 
                     response = sr.ReadLine();
-                    Console.WriteLine("Otrzymano odpowiedź: " + response);
-                } while (!message.Equals("exit"));
+                    Console.WriteLine("[Received] " + response);
+                } while (!message.Equals("exit", StringComparison.OrdinalIgnoreCase));
             }
         }
 
-        Console.WriteLine("Zakończono.");
+        Console.WriteLine("Disconnected.");
     }
 }
