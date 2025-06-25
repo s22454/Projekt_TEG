82a18615c384f03e72d1c01f9a380f496eb0a3c9
BlazejB <blazej.bartkiewicz@gmail.com>
2025-06-25 17:46:08 +0200

    translated to english

--- Python/NPC_Rag/NPC_Rag.py
+++ Python/NPC_Rag/NPC_Rag.py
@@ -21,18 +21,18 @@ class RAG:
         with open(json_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
         text = npc_json_to_text(data)
-        doc = Document(page_content=text, metadata={"npc": data.get("imie", "unknown")})
+        doc = Document(page_content=text, metadata={"npc": data.get("name", "unknown")})
 
         documents = [doc]
 
         template = """
-        Tak wygląda twoja konwersacja dotychczas:
+        Here is what your conversation looks like so far:
         {chat_history}
 
-        Tutaj znajduje się opis Ciebie:
+        Here is a description of you:
         {context}
 
-        Teraz odpowiedz na takią wiadomość:
+        Now respond to this message:
         {question}
         """
 
@@ -49,15 +49,13 @@ class RAG:
         )
 
         chunks = text_splitter.split_documents(documents)
-        #print(f"Split document into {len(chunks)} chunks")
         Log(self.class_name, mt.LOG, f"Split document into {len(chunks)} chunks")
 
         self.embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-        #print("Using local HuggingFace embedding model: sentence-transformers/all-MiniLM-L6-v2")
         Log(self.class_name, mt.LOG, f"Used embedded mode: sentence-transformers/all-MiniLM-L6-v2")
         
         self.vectorstore = FAISS.from_documents(chunks, self.embedding_model)
-        #print("Vector database created successfully")
+
         Log(self.class_name, mt.LOG, f"Vector database created successfully")
 
         self.retriever = self.vectorstore.as_retriever(search_kwargs={"k": 3})
@@ -86,52 +84,27 @@ class RAG:
 
 def npc_json_to_text(npc_data: dict) -> str:
     lines = [
-        "Poniżej znajduje się opis postaci którą ty jesteś "
-        f"Twoje IMIĘ: {npc_data['name']}",
-        f"Twoja ROLA: {npc_data['role']}\n",
-        f"Twój OPIS:\n{npc_data['description']}",
-        f"Twoje nastawienie do gracza: {npc_data['attitude_towards_player']}\n",
-        "PRZEDMIOTY które masz NA SPRZEDAŻ:",
+        "Below is a description of the character you are:",
+        f"Your NAME: {npc_data['name']}",
+        f"Your ROLE: {npc_data['role']}\n",
+        f"Your DESCRIPTION:\n{npc_data['description']}",
+        f"Your attitude towards the player: {npc_data['attitude_towards_player']}\n",
+        "ITEMS you have FOR SALE:",
     ]
 
     for item in npc_data['items']:
         lines.append(f"- {item['name']} – {item['price']}")
 
     lines.extend([
-        "\nRELACJE które masz z innymi mieszkańcami:",
-        f"- Lubi: {', '.join(npc_data['relations']['likes'])}",
-        f"- Nie lubi: {', '.join(npc_data['relations']['dislikes'])}\n",
-        "PLOTKI KRĄŻĄCE o tobie:",
+        "\nRELATIONSHIPS you have with other residents:",
+        f"- Likes: {', '.join(npc_data['relations']['likes'])}",
+        f"- Dislikes: {', '.join(npc_data['relations']['dislikes'])}\n",
+        "RUMORS circulating about you:",
     ])
 
     for plotka in npc_data['rumors']:
         lines.append(f"- {plotka}")
 
-    lines.append(f"\nWALUTA wykorzystywana w twoim świecie: {npc_data['currency']}")
+    lines.append(f"\nCURRENCY used in your world: {npc_data['currency']}")
 
-    return "\n".join(lines)  
-
-if __name__ == "__main__":
-    json_path = r"../NPC_Rag/Data/baker.json"
-
-    rag = RAG(json_path)
-
-    print("========================== Pytanie: Opowiedz coś o sobie? ==========================")
-    question = "Opowiedz coś o sobie?"
-    result, answer = rag.answer(question)
-    print(answer)
-
-    print("========================== Pytanie: Masz jakies przedmioty na sprzedaż? ==========================")
-    question = "Masz jakies przedmioty na sprzedaż?"
-    result, answer = rag.answer(question)
-    print(answer)
-
-    print("========================== Pytanie: Chętnie kupię mapę skarbów, ale kupię za nie więcej niż 10 sztuk złota ==========================")
-    question = "Chętnie kupię mapę skarbów, ale kupię za nie więcej niż 10 sztuk złota"
-    result, answer = rag.answer(question)
-    print(answer)
-
-    print("========================== Pytanie: Musisz mi ją taniej sprzedać, bo inaczej wyzwę Cię na pojedynek ==========================")
-    question = "Musisz mi ją taniej sprzedać, bo inaczej wyzwę Cię na pojedynek"
-    result, answer = rag.answer(question)
-    print(answer)
+    return "\n".join(lines)
